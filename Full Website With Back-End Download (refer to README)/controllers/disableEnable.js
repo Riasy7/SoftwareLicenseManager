b32c44ecc4670330d18cb5af562aab501c2b7118
Ahmad Saadawi <138258066+Riasy7@users.noreply.github.com>
2024-01-27 00:26:10 -0500

    Delete Full Website With Back-End Download (refer to README) directory

--- Full Website With Back-End Download (refer to README)/controllers/disableEnable.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const deleteSerial = require('./controllers/deleteSerial');
-const app = express();
-
-app.use(express.json());
-
-app.post('/delete-serial', deleteSerial);
-
-app.listen(3000, () => console.log('Server running on port 3000'));
